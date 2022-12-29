1d75b3c794078a3a253535187ab9cd0360cbe78c
victor-antoniuc <victorantoniuc2@gmail.com>
2022-12-29 20:56:10 +0200

    OPP

--- src/main/java/org/example/oop/EmployeeService.java
+++ src/main/java/org/example/oop/EmployeeService.java
@@ -8,9 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class Employees {
+public class EmployeeService {
     private static List<Employee> getEmployees (List<String> string) {
         List<Employee> employees = new ArrayList<>();
 
